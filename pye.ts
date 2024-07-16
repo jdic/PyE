2f03be7297c9cacdfb1cd34fbdbd96d6cc364a89
jdi <jmotadv@gmail.com>
2024-07-15 17:32:09 -0700

    [FEAT] Operation selection menu

---
+++ pye.ts
@@ -0,0 +1,31 @@
+import select, { Separator } from '@inquirer/select';
+import { Z } from './logic/Z'
+import 'colors'
+
+console.clear()
+
+const choices: any =
+[
+  {
+    name: 'Z',
+    value: Z
+  },
+  new Separator(),
+  {
+    name: 'Exit',
+    value: 'exit'
+  },
+]
+
+;(async () =>
+{
+  while (true)
+  {
+    const answer = await select({ message: 'Option', choices })
+
+    if (typeof answer === 'function')
+      await answer()
+    else if (answer === 'exit')
+      process.exit(0)
+  }
+})()
